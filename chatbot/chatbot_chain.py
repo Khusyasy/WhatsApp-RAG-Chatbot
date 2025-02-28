cd4dc21a317963c0432f06f30c2fd79392cf61ea
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2025-02-28 11:42:36 +0700

    feat: create chat history handler

--- chatbot/chatbot_chain.py
+++ chatbot/chatbot_chain.py
@@ -1,10 +1,11 @@
-import os
 import time
 
+from chat_history import get_session_history
 from dotenv import load_dotenv
-from langchain.chains import create_retrieval_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 from transformers import pipeline
 from vector_database import create_vector_database
@@ -27,6 +28,20 @@ def create_chatbot_chain():
 
     retriever = create_vector_database()
 
+    # chain buat bikin ulang pertanyaan sesuai konteks
+    contextualize_q_system_prompt = "Diberikan riwayat obrolan dan pertanyaan user terbaru yang mungkin merujuk pada konteks pada riwayat obrolan, rumuskan pertanyaan mandiri yang dapat dipahami tanpa riwayat obrolan. JANGAN menjawab pertanyaan tersebut, cukup rumuskan ulang jika diperlukan dan kembalikan apa adanya."
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        chat_model, retriever, contextualize_q_prompt
+    )
+
+    # chain buat jawaban
     qa_system_prompt = (
         "Anda adalah asisten chatbot WhatsApp personal. "
         "Anda harus menjawab pertanyaan dengan akurat dan relevan berdasarkan konteks. "
@@ -38,14 +53,22 @@ def create_chatbot_chain():
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
             ("human", "{input}"),
         ]
     )
     question_answer_chain = create_stuff_documents_chain(chat_model, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
-    # TODO: handle chat history
-    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-    return rag_chain
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+
+    return conversational_rag_chain
 
 
 if __name__ == "__main__":
@@ -55,7 +78,10 @@ if __name__ == "__main__":
         if message == "exit":
             break
         starttime = time.perf_counter_ns()
-        res = chain.invoke({"input": message})
+        res = chain.invoke(
+            {"input": message},
+            config={"configurable": {"session_id": "abc123"}},
+        )
         endtime = time.perf_counter_ns()
         totaltime = (endtime - starttime) / 1_000_000_000
         # print(res)
